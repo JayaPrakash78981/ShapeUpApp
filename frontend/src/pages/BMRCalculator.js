1a07330e7e2f49b61dc71771eb4ba87c540dfb87
JayaPrakash78981 <jaiprakashkoka@gmail.com>
2025-11-08 19:36:44 +0530

    Update BMRCalculator.js

--- frontend/src/pages/BMRCalculator.js
+++ frontend/src/pages/BMRCalculator.js
@@ -1,24 +1,23 @@
 import React from "react";
-import { Container, Row, Col } from "react-bootstrap";
+import { /* Removed Container, Row, Col */ } from "react-bootstrap";
 import CalorieCalculator from "../components/CalorieCalculator";
 import FormContainer from "../components/FormContainer";
 import Footer from "../components/Footer";
 
 const BMRCalculator = () => {
-  return (
-    <>
-      {/* We use the same 'page-container' for a consistent look */}
-      <div className="page-container">
-        <FormContainer>
-          {/* FormContainer gives us the professional card style */}
-          <h2 className="text-center" style={{ color: '#ffffff' }}>BMR Calculator</h2>
-          <CalorieCalculator />
-        </FormContainer>
-      </div>
-      <Footer />
-    </>
-  );
+  return (
+    <>
+      {/* We use the same 'page-container' for a consistent look */}
+      <div className="page-container">
+        <FormContainer>
+          {/* FormContainer gives us the professional card style */}
+          <h2 className="text-center" style={{ color: '#ffffff' }}>BMR Calculator</h2>
+          <CalorieCalculator />
+        </FormContainer>
+      </div>
+      <Footer />
+    </>
+  );
 };
 
 export default BMRCalculator;
-
